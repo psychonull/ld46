eb89612dd281191e5646a09a9eace6591b328190
Matias Arriola <matias.arriola@gmail.com>
2020-04-20 19:58:09 -0300

    Add game presets + config

--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import GUI from '/scenes/GUI';
 import GameOver from '/scenes/GameOver';
 
 import InputManagerPlugin from '/plugins/inputManager';
+import configs from '/configs';
+import {
+  loadConfig,
+  saveConfig,
+  presetsToForm,
+  configToForm,
+  formToConfig
+} from '/utils/config';
+
+window.gameConfig = loadConfig();
 
 const config = {
   type: Phaser.AUTO,
@@ -38,6 +48,7 @@ const config = {
 const game = new Phaser.Game(config);
 
 const helpPopup = document.getElementById('help-popup');
+const configPopup = document.getElementById('config-popup');
 
 window.document.getElementById('help-button').addEventListener('click', (e) => {
   e.preventDefault();
@@ -45,10 +56,37 @@ window.document.getElementById('help-button').addEventListener('click', (e) => {
 });
 
 window.document
+  .getElementById('config-button')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+    configPopup.classList.remove('hidden');
+  });
+
+window.document
   .getElementById('help-close-button')
   .addEventListener('click', (e) => {
     e.preventDefault();
     helpPopup.classList.add('hidden');
   });
+
+window.document
+  .getElementById('config-cancel-button')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+    configPopup.classList.add('hidden');
+  });
+
+window.document
+  .getElementById('config-save-button')
+  .addEventListener('click', (e) => {
+    e.preventDefault();
+    const newConfig = formToConfig();
+    saveConfig(newConfig);
+    window.gameConfig = newConfig;
+    configPopup.classList.add('hidden');
+  });
+
+presetsToForm(configs);
+configToForm(window.gameConfig);
 
 export default game;
