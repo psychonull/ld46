193a0863c1688c57e803afe02f5613fb663e2841
Pablo Novas <pjnovas@gmail.com>
2020-04-20 21:10:27 -0300

    added loader

--- src/scenes/World.js
+++ src/scenes/World.js
@@ -38,6 +38,73 @@ class World extends Phaser.Scene {
   }
 
   preload() {
+    this.cameras.main.setBackgroundColor('#041015');
+
+    // PROGRESS BAR ------------------------------------
+    var progressBar = this.add.graphics();
+    progressBar.fillStyle(0x041015, 1);
+    var progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+
+    var width = this.cameras.main.width;
+    var height = this.cameras.main.height;
+
+    progressBox.fillRect(350, 270, 320, 50);
+    var loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'Loading...',
+      style: {
+        font: '20px monospace',
+        fill: '#ffffff'
+      }
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    var percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    var assetText = this.make.text({
+      x: width / 2,
+      y: height / 2 + 50,
+      text: '',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff'
+      }
+    });
+
+    assetText.setOrigin(0.5, 0.5);
+
+    this.load.on('progress', function (value) {
+      percentText.setText(parseInt(value * 100) + '%');
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(360, 280, 300 * value, 30);
+    });
+
+    this.load.on('fileprogress', function (file) {
+      assetText.setText('Loading asset: ' + file.key);
+    });
+
+    this.load.on('complete', function () {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+      assetText.destroy();
+    });
+
+    // ------------------------------------
+
     this.load.image('white_square', '/ld46/images/white_square.png');
     this.load.image('grid', '/ld46/images/grid.png');
 
